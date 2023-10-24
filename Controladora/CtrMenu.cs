db97cae0ebb3d4cde74386db4c850b2266810135
Jesús <jesus_mechan_gonzales@hotmail.com>
2023-10-24 01:36:30 -0500

    Implementación dapper

--- Controladora/CtrMenu.cs
+++ Controladora/CtrMenu.cs
@@ -11,14 +11,50 @@ namespace Controladora
     public class CtrMenu
     {
         DaoMenu _dao = null;
+        ClaseResultado<DtoMenu> _resultado = null;
         public CtrMenu()
         {
             _dao = new DaoMenu();
+            _resultado = new ClaseResultado<DtoMenu>();
         }
 
         public List<DtoMenu> Menu_Listar(DtoMenu entidad)
         {
             return _dao.Menu_Listar(entidad);
+        }
+
+        public List<DtoMenu> Menu_Listar_Tabla()
+        {
+            return _dao.Menu_Listar_Tabla();
+        }
+
+        public ClaseResultado<DtoMenu> Menu_Insertar_Actualizar(DtoMenu _entidad)
+        {
+            try
+            {
+                _resultado = _dao.Menu_Insertar_Actualizar(_entidad);
+            }
+            catch (Exception ex)
+            {
+                _resultado.HuboError = true;
+                _resultado.Mensaje = ex.Message.ToString();
+
+            }
+            return _resultado;
+        }
+
+        public ClaseResultado<DtoMenu> Menu_Activar_Inactivar(DtoMenu _entidad)
+        {
+            try
+            {
+                _resultado = _dao.Menu_Activar_Inactivar(_entidad);
+            }
+            catch (Exception ex)
+            {
+                _resultado.HuboError = true;
+                _resultado.Mensaje = ex.Message.ToString();
+            }
+            return _resultado;
         }
     }
 }
